bcde99eaabba56e11fc479ed492b534577c4d63f
CarloDettori <dettoric07@gmail.com>
2026-01-28 19:31:01 +0100

    fixed server bugs

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,30 @@
-import mysql from 'mysql2';
+import 'dotenv/config';
+import mysql from 'mysql2/promise';
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error("Connection to DataBase Failed:", err.message);
-        return process.exit(1); // Termina il server con errore
-    } else {
-        console.log("Connection to database completed!");
-    }
+console.log('ENV CHECK:', {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    db: process.env.DB_NAME
 });
 
-export default db
+try {
+    const connection = await db.getConnection();
+    console.log('Connection to database completed!');
+    connection.release();
+} catch (err) {
+    console.error('Connection to DataBase Failed:', err.message);
+    process.exit(1);
+}
+
+export default db;
